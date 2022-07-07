6a443c1403fd2717ddb2a9b9d701368121f8700a
Костя <kostyaopanasuk@gmail.com>
2022-07-07 22:57:35 +0300

    header

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -4,6 +4,7 @@ import HelpBlock from '../../components/HelpBlock';
 import Map from '../../components/Map';
 import ModelRow from '../../components/ModelRow';
 import NewsBlock from '../../components/NewsBlock';
+import PartnersBlock from '../../components/PartnersBlock';
 
 const Home: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const Home: React.FC = () => {
       <FirstScreenHomePage />
       <ModelRow />
       <Map />
+      <PartnersBlock />
       <HelpBlock />
       <NewsBlock />
     </>
